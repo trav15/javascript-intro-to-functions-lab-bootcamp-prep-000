ef01546eaeb04a731db147718e4bb9b38e8d1629
Travis Tokuyama <travistokuyama@gmail.com>
2018-07-10 13:36:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string) {
     return "I can't hear you!"
   }  else if (string.toUpperCase() === string) {
-        return "YES INDEED!"
+    return "YES INDEED!"
   }
 }
